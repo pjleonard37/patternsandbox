c077800c40ce1da7dbe47b3457dc15cec49f1086
PJ Leonard <patrick@messageagency.com>
2015-10-08 12:26:48 -0400

    Added in revealing module pattern

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,25 @@
 console.log("loaded main.js");
+
+// Revealing Module Pattern
+// Example from https://carldanley.com/js-revealing-module-pattern/
+var MyModule = (function(window, undefined){
+	
+	function myMethod(){
+		alert('my Method');
+	}
+
+	function myOtherMethod(){
+		alert('my other Method');
+	}
+
+	//return public methods explicitly
+	return{
+		someMethod: myMethod,
+		someOtherMethod: myOtherMethod
+	};
+})(window);
+
+MyModule.myMethod();
+MyModule.myOtherMethod();
+MyModule.someMethod();
+MyModule.someOtherMethod();

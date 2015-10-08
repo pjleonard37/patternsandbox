26aa2f56df070538a99cb907860f93a7059bce15
PJ Leonard <patrick@messageagency.com>
2015-10-08 13:35:13 -0400

    Changing to obj literal pattern

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,6 +2,8 @@ console.log("loaded main.js");
 
 // Revealing Module Pattern
 // Example from https://carldanley.com/js-revealing-module-pattern/
+
+/*
 var MyModule = (function(window, undefined){
 	
 	function myMethod(){
@@ -18,8 +20,27 @@ var MyModule = (function(window, undefined){
 		someOtherMethod: myOtherMethod
 	};
 })(window);
+*/
 
 //MyModule.myMethod();
 //MyModule.myOtherMethod();
-MyModule.someMethod();
-MyModule.someOtherMethod();
+//MyModule.someMethod();
+//MyModule.someOtherMethod();
+
+// Modular JS with Object Literal Pattern
+// From: https://www.youtube.com/watch?v=HkFlM73G-hk&list=PLoYCgNOIyGABs-wDaaxChu82q_xQgUb4f
+
+// object Literal
+var MyModule = {
+	name: 'PJ',
+	year: 2014,
+	sayName: function(){
+		console.log(this.name);
+	},
+	setName: function(newName){
+		this.name = newName;
+	}
+};
+
+myModule.setName('Raphael');
+myModule.sayName();

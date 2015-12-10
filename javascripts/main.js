9695a02a912a1900d9b90f18dc392f03a894765f
PJ Leonard <patrick@messageagency.com>
2015-12-10 15:30:31 -0500

    opps

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,7 +7,7 @@ console.log("loaded main.js");
 
 (function() {
   console.log("function sent");
-  var spreadsheetID = "13SN1M6K1lbkmME6b5x1rigC2ps52w3NNqjMX7cnGTGU";
+  var spreadsheetID = ""
   var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json-in-script&callback=";
   console.log(url)
   $.getJSON(url, function(data) {

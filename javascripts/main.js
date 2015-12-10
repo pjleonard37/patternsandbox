d1b245646d1425e5d0bf936a811642154de05839
PJ Leonard <patrick@messageagency.com>
2015-12-10 14:31:35 -0500

    functionc

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,16 +5,16 @@ var spreadsheetID = "13SN1M6K1lbkmME6b5x1rigC2ps52w3NNqjMX7cnGTGU";
 
 // Make sure it is public or set to Anyone with link can view
 var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
-
-$.getJSON(url, function(data) {
- console.log(data);
- var entry = data.feed.entry;
- console.log(entry);
- $(entry).each(function(){
-   // Column names are name, age, etc.
-   $('.results').prepend('<h2>'+this.gsx$name.$t+'</h2><p>'+this.gsx$age.$t+'</p>');
- });
-
+(function() {
+  $.getJSON(url, function(data) {
+    console.log(data);
+    var entry = data.feed.entry;
+    console.log(entry);
+    $(entry).each(function(){
+     // Column names are name, age, etc.
+     $('.results').prepend('<h2>'+this.gsx$name.$t+'</h2><p>'+this.gsx$age.$t+'</p>');
+    });
+  });
 })();
 
 

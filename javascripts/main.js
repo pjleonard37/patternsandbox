67de1251bf330e54ff7d596d5e761e373062c936
PJ Leonard <patrick@messageagency.com>
2015-12-11 13:02:11 -0500

    fiddling

--- javascripts/main.js
+++ javascripts/main.js
@@ -10,25 +10,25 @@ console.log("loaded main.js");
   var spreadsheetID = "140f-u3YxCg3xlspx-xNWPbkXVPSHOOMopCWtsV-Wdao"
   var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
   console.log(url)
-  $.getJSON(url, function(data) {
-    var entry = data.feed.entry;
-    console.log(entry);
-    $(entry).each(function(){
-      var chart = c3.generate({
-          bindto: '#chart',
-          data: {
-            entry,
-      			type: 'pie'
-          },
-          color: {
-            pattern: ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
-          },
-          legend: {
-              position: 'bottom'
-          }
-      });
-    });
+  // $.getJSON(url, function(data) {
+  //   var entry = data.feed.entry;
+  //   console.log(entry);
+  //   $(entry).each(function(){
+  var chart = c3.generate({
+      bindto: '#chart',
+      data: {
+        url,
+  			type: 'pie'
+      },
+      color: {
+        pattern: ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
+      },
+      legend: {
+          position: 'bottom'
+      }
   });
+    // });
+  // });
 })();
 
 

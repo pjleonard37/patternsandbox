5f828b1def3796878c212ff2e81ec7ecc9de4acb
PJ Leonard <patrick@messageagency.com>
2015-12-11 14:27:22 -0500

    data

--- javascripts/main.js
+++ javascripts/main.js
@@ -15,7 +15,7 @@ console.log("loaded main.js");
     var datap = [];
     console.log(entry);
     for (var i = 0; i < entry.length; i++) {
-      datap.push([entry[i].gsx$name.$t, entry[i].gsx$percentage.$t]);
+      datap.push([entry[i].2.$t, entry[i].3.$t]);
     }
     console.log(datap);
     var chart = c3.generate({

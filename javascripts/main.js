f613633b6339608c99824f48f51f70c0e786b916
PJ Leonard <patrick@messageagency.com>
2015-12-11 15:07:31 -0500

    types

--- javascripts/main.js
+++ javascripts/main.js
@@ -35,18 +35,18 @@ console.log("loaded main.js");
     //   });
     // });
     function (data) {
-    for (var i = 0; i < data.feed.entry.length; i++) {
-        for (var key in data.feed.entry[i]) {
-            if (data.feed.entry[i].hasOwnProperty(key) && key.substr(0,4) === 'gsx$') {
-                // copy the value in the key up a level and delete the original key
-                data.feed.entry[i][key.substr(4)] = data.feed.entry[i][key].$t;
-                delete data.feed.entry[i][key];
-            }
-        }
-    }
-    console.log(data.feed.entry);
-    return data.feed.entry;
-}
+      for (var i = 0; i < data.feed.entry.length; i++) {
+          for (var key in data.feed.entry[i]) {
+              if (data.feed.entry[i].hasOwnProperty(key) && key.substr(0,4) === 'gsx$') {
+                  // copy the value in the key up a level and delete the original key
+                  data.feed.entry[i][key.substr(4)] = data.feed.entry[i][key].$t;
+                  delete data.feed.entry[i][key];
+              }
+          }
+      }
+      console.log(data.feed.entry);
+      return data.feed.entry;
+    });
 })();
 
 

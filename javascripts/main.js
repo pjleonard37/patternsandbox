438c193009d583d1624e2d8bfd19609b39d737aa
PJ Leonard <patrick@messageagency.com>
2015-12-14 16:30:34 -0500

    programmatic

--- javascripts/main.js
+++ javascripts/main.js
@@ -12,10 +12,12 @@ console.log("loaded main.js");
   console.log(url)
   $.getJSON(url,
     function (data) {
+      var xaxisoptions = [];
       for (var i = 0; i < data.feed.entry.length; i++) {
           for (var key in data.feed.entry[i]) {
               if (data.feed.entry[i].hasOwnProperty(key) && key.substr(0,4) === 'gsx$') {
                   data.feed.entry[i][key.substr(4)] = data.feed.entry[i][key].$t;
+                  xaxisoptions.push(data.feed.entry[i][key.substr(4)]);
                   delete data.feed.entry[i][key];
               }
               else
@@ -27,11 +29,6 @@ console.log("loaded main.js");
       var datap = data.feed.entry;
       console.log(datap);
       var xaxistitle = datap[0][0];
-      var xaxisoptions = [];
-      for (i = 1; i < datap[0].length; i++)
-      {
-        xaxisoptions.push(datap[0][i]);
-      }
       console.log(xaxistitle);
       console.log(xaxisoptions);
       var chart = c3.generate({

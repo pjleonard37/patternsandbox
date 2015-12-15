2dfc6daee76f02e01bc9e721f06307bd346ceaa2
PJ Leonard <patrick@messageagency.com>
2015-12-15 15:09:16 -0500

    line

--- javascripts/main.js
+++ javascripts/main.js
@@ -43,9 +43,9 @@ console.log("loaded main.js");
               value: xaxisoptions
             },
             names: categorynames,
-            type: 'bar',
-            order: null,
-            groups: [xaxisoptions]
+            //type: 'bar',
+            //order: null,
+            //groups: [xaxisoptions]
           },
           color: {
             pattern: ['#449AA2', '#364D6E', '#703F7D', '#ED871F', '#B5004A', '#744584', '#cfddb8', '#97AFD2']

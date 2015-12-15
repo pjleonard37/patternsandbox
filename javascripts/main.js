451cdf48f530c7def267c6ada6853bb4879e77b0
PJ Leonard <patrick@messageagency.com>
2015-12-15 13:11:38 -0500

    google bugs

--- javascripts/main.js
+++ javascripts/main.js
@@ -43,7 +43,7 @@ console.log("loaded main.js");
             json: datafeed,
             keys: {
               x: xaxisoptions[0],
-              value: xaxisoptions
+              //value: xaxisoptions
             },
             type: 'bar'
           },
